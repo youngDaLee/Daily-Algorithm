6158183104848adb1424637006983ae1fc10ad88
YoungDaLee <ldy0956@gmail.com>
2021-01-24 17:55:55 +0900

    스택/큐

--- programmers/stack_queue/주식가격.py
+++ programmers/stack_queue/주식가격.py
@@ -1,17 +1,17 @@
 def solution(prices):
     answer = []
-    stack = []
+
     for i in range(0, len(prices)):
         cnt = 0
-        stack.append(prices[i])
         for j in range(i, len(prices)):
-            if stack[-1] > prices[j] or j+1==len(prices) :
+            if prices[i] > prices[j] or j+1 == len(prices):
                 answer.append(cnt)
                 break
             cnt = cnt+1
 
     return answer
 
-prices = [1,2,3,2,3]
+
+prices = [1, 2, 3, 2, 3]
 ans = solution(prices)
 print(ans)
