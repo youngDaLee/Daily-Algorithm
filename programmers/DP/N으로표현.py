65928317d6beae20168bb5d493ae20a609e745f3
YoungDaLee <ldy0956@gmail.com>
2021-02-05 21:58:34 +0900

    dp

--- programmers/DP/N으로표현.py
+++ programmers/DP/N으로표현.py
@@ -1,3 +1,17 @@
 def solution(N, number):
     answer = 0
+
+    dp = []
+    dp.append([N])
+    for i in range(1, 8):
+        dp.append([dp[i-1][0]+N*10**i])
+
+    for i in range(8):
+        pass
+
     return answer
+
+
+N = 5
+number = 12
+print(solution(N, number))
