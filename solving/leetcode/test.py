42ad5d09f6bfd02947c08b49e384f6eb214d060e
youngDaLee <ldy0956@gmail.com>
2021-03-09 16:42:43 +0900

    leetcode

--- solving/leetcode/test.py
+++ solving/leetcode/test.py
@@ -1,2 +1,29 @@
-n, m, v = map(int, input("n입력 : ").split())
-print(n,m,v)
+from collections import deque
+
+def removePalindromeSub(s: str):
+    answer = 0
+    stack = s
+    li = deque(s)
+    while stack:
+        stack = []
+        l = 0
+        r = len(li)-1
+        
+        while l<=r:
+            if l == r:
+                li.popleft()
+                break
+            if li[l] == li[r]:
+                li.popleft()
+                li.pop()
+            else:
+                stack.append(li.pop())
+            r = len(li)-1
+        answer += 1
+        li = deque(stack)
+        
+    return answer 
+
+
+s = "abbaaaab"
+print(removePalindromeSub(s))
